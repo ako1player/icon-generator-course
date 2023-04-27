51805a74e62fd9e0007b75b08daf5bded7b2827d
Adrian Garcia Rios <agarciar@GMU.EDU>
2023-04-26 23:44:58 -0400

    adding first mutation endpoint and sending data from form ui

--- src/component/input.tsx
+++ src/component/input.tsx
@@ -1,3 +1,3 @@
 export function Input(props: React.ComponentPropsWithoutRef<"input">){
-    return <input {...props} type="text" className="border border-gray-800"></input>
+    return <input {...props} type="text" className="border border-gray-800 dark:text-gray-800 px-4 py-2 rounded"></input>
 }
--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,10 +1,35 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import { FormGroup } from "~/component/FormGroup";
 import { Input } from "~/component/input";
+import { api } from "~/utils/api";
 
 const GeneratePage: NextPage = () => {
  
+  const [form, setForm] = useState({
+    prompt: "",
+  })
+
+  const generateIcon = api.generate.generateIcon.useMutation();
+
+  function handleFormSubmit(e: React.FormEvent){
+    e.preventDefault();
+    // TODO: submit form data to the backend
+     generateIcon.mutateAsync({
+      prompt: form.prompt,
+     })
+  }
+
+  function updateForm(key: string){
+    return function(e: React.ChangeEvent<HTMLInputElement>){
+      setForm((prev) =>({
+        ...prev,
+        [key]: e.target.value,
+      }))
+    }
+  }
+
   return (
     <>
       <Head>
@@ -13,10 +38,10 @@ const GeneratePage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleFormSubmit}>
             <FormGroup>
                 <label>Prompt</label>
-                <Input></Input>
+                <Input onChange={updateForm("prompt")} value={form.prompt}></Input>
             </FormGroup>
             <button className="bg-blue-400 hover:bg-blue-500 px-4 py-2 rounded">Submit</button>
         </form>
